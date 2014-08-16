0f2efe989a660111079136cd2e271f9dfb0b5197
Jens Schutt <jens@eviking.com>
2014-08-16 22:24:15 +0000

    fixed stuff

--- start.sh
+++ start.sh
@@ -1,12 +1,6 @@
 cd /tmp
-
-# try to remove the repo if it already exists
 rm -rf mydockerapp; true
-
 git clone https://github.com/eviking/mydockerapp.git
-
 cd mydockerapp
-
 npm install
-
-node .
+/usr/bin/nodejs index.js
