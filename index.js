961c663fd1d8f51efd75350b06971d784beec94c
Jens Schutt <jens@eviking.com>
2014-08-16 22:32:49 +0000

    fixed stuff

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Hello from inside a container!');
+    res.send('Hello from inside a github based container!');
 });
 
 app.listen(8080);
