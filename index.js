e15da13bd01e3d53582eb9549fe8539d5d387d3b
Jens Schutt <jens.schutt@joyent.com>
2015-03-05 16:52:35 -0800

    trying dot notation to get a perfect score

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 app.set('counter',0);
 app.get('/', function(req, res){
     app.set('counter',app.get('counter')+1);
-    res.send('{ "requestCounter":' + app.get('counter') + ', "hostname":' + os.hostname() + ' ,"dockerContainerName":' + process.env["HOSTNAME"]/* +  ' ,"mesos-port":' + process.env["PORT0"]*/ + '}');
+    res.send('{ "requestCounter":' + app.get('counter') + ', "hostname":' + os.hostname() + ' ,"dockerContainerName":' + process.env.HOSTNAME/* +  ' ,"mesos-port":' + process.env["PORT0"]*/ + '}');
 });
 
 app.listen(8089);
